696f0fc07337fa712d5339fc4c9f29a78f025eb6
Freda-Marie Efua Beecham <fredamariabeech@gmail.com>
2024-02-21 10:42:30 +0000

    added authentication pages

---
+++ login/signup_view.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!-- Coding By CodingNepal - codingnepalweb.com -->
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title> Signup</title>
+    <link rel="stylesheet" href="../css/signup_style.css" />
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="registration form">
+            <header>Signup</header>
+            <form action="#">
+                <input type="text" placeholder="Enter your Student ID" required name="studID" />
+
+                <input type="text" placeholder="Enter your email" name="email">
+                <input type="password" placeholder="Create a password" name="passwrd">
+                <input type="password" placeholder="Confirm your password" name="cpasswrd">
+                <input type="button" class="button" value="Signup">
+            </form>
+            <div class="signup">
+                <span class="signup">Already have an account?
+                    <label for="check">Login</label>
+                </span>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
