7f694e52e0b8b971123be034eda29170ff947ec4
Freda-Marie Efua Beecham <fredamariabeech@gmail.com>
2024-02-22 23:12:13 +0000

    added new files

---
+++ view/item_found.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Found Items</title>
+
+</head>
+<body>
+
+        <div class="overall">
+          <div class="side-menu">
+              <h2>Menu</h2>
+              <p>The things on the menu go here.</p>
+          </div>
+      
+          
+          <div class="items">
+              <h2>Items Found</h2> <!--Center align it-->
+
+              <div class="search-bar">
+                 <input type="text" placeholder="Search" style="float: right;">
+              </div>
+  
+              <!--<div> --->
+                  <label for="sort-by">Sort By:</label>
+                  <select id="sort-by">
+                      <option value="name">Name</option>
+                      <option value="price">Price</option>
+                  </select>
+              <!---</div>---->
+              <span>Items per page: 15</span> <!---Align this to the right-->
+  
+              <div class="lost">
+                  <div class="item">
+                      <img src="pic.png" alt="" class="image">
+                      <h3>Name</h3>  <!--There should be a link to redirect to the full page with description-->
+                      <p>Replace with tags</p>
+                  </div>
+                  <div class="item">
+                      <img src="" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div>
+                  <div class="item">
+                      <img src="" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div><div class="item">
+                      <img src="pic.png" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div>
+                  <div class="item">
+                      <img src="" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div>
+                  <div class="item">
+                      <img src="" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div><div class="item">
+                      <img src="pic.png" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div>
+                  <div class="item">
+                      <img src="" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div>
+                  <div class="item">
+                      <img src="" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div><div class="item">
+                      <img src="pic.png" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div>
+                  <div class="item">
+                      <img src="" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div>
+                  <div class="item">
+                      <img src="" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div><div class="item">
+                      <img src="pic.png" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div>
+                  <div class="item">
+                      <img src="" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div>
+                  <div class="item">
+                      <img src="" alt="" class="image">
+                      <h3>Name</h3>
+                      <p>Replace with tags</p>
+                  </div>
+              </div>
+  
+              <div class="pagination">
+                  <a href="#">&laquo;</a>
+                  <a href="#">1</a>
+                  <a class="active" href="#">2</a>
+                  <a href="#">3</a>
+                  <a href="#">4</a>
+                  <a href="#">5</a>
+                  <a href="#">6</a>
+                  <a href="#">&raquo;</a> 
+                </div>
+                
+  
+          </div>
+      
+      </div>
+  
+</body>
+</html>
