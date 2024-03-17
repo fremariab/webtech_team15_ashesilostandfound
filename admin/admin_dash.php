79150d99d802ca77433d5957062b07e094a53cef
kiki-etc <kiki.etc@icloud.com>
2024-03-17 14:26:17 +0000

    edited Admin dashboard, given template

--- admin/admin_dash.php
+++ admin/admin_dash.php
@@ -1,192 +1,87 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <!-- Boxicons -->
-    <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
-    <!-- My CSS -->
+    <title>User Dashboard</title>
     <link rel="stylesheet" href="../css/admin_dash_style.css">
 
-    <title>Lost and Found Admin Dashboard</title>
+
 </head>
-
 <body>
-    <section id="sidebar">
-        <a href="#" class="brand">
-            <span class="text">Ashesi Lost and Found</span>
-        </a>
-        <ul class="side-menu top">
-            <li class="active">
-                <a href="#">
-                    <i class='bx bxs-dashboard'></i>
-                    <span class="text">Dashboard</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class='bx bxs-add-to-queue'></i>
-                    <span class="text">Reported Items</span>
-                </a>
-            </li>
-            <li>
-                <a href="#">
-                    <i class='bx bxs-check-shield'></i>
-                    <span class="text">Recovered Items</span>
-                </a>
-            </li>
+    <div class = "dashboard-container">
+        <aside class = "sidebar">
+            <div class = "sidebar-logo">
+                <img id = "Logo" src = "Ashesi-logo.png">
+            </div>
+            <div class="sidebar-top">
+                <a href="Submission.html"> Dashboard</a>
+                <a href="Submission.html"> Show all lost items</a>
+                <a href="Submission.html"> Show found items</a>
+                <a href="Submission.html"> Change status</a>
+            </div>
+            
+            <div class="sidebar-bottom">
+                <hr>
+                <a href="Submission.html"> Profile</a>
+                <a href="Submission.html"> Logout</a>
+            </div>
 
+        </aside>
 
-        </ul>
-        <ul class="side-menu">
+        <main class = "main-content">
+            <header class = "header">
+                <h1>Dashboard</h1>
+                <p>Admin Name</p>
+            </header>
 
-            <li>
-                <a href="#" class="logout">
-                    <i class='bx bxs-log-out-circle'></i>
-                    <span class="text">Logout</span>
-                </a>
-            </li>
-        </ul>
-    </section>
-    <!-- SIDEBAR -->
-
-
-
-    <!-- CONTENT -->
-    <section id="content">
-        <!-- NAVBAR -->
-        <nav>
-
-            <form action="#">
-                <div class="form-input">
-                    <input type="search" placeholder="Search...">
-                    <button type="submit" class="search-btn"><i class='bx bx-search'></i></button>
-                </div>
-            </form>
-
-            <a href="#" class="notification">
-                <i class='bx bxs-bell'></i>
-                <span class="num">8</span>
-            </a>
-            <a href="#" class="profile">
-                <img src="img/people.png">
-            </a>
-        </nav>
-        <!-- NAVBAR -->
-
-        <!-- MAIN -->
-        <main>
-            <div class="head-title">
+            <div class="subheader">
                 <div class="left">
-                    <h1>Dashboard</h1>
-                    <ul class="breadcrumb">
-                        <li>
-                            <a href="#">Dashboard</a>
-                        </li>
-                        <li><i class='bx bx-chevron-right'></i></li>
-                        <li>
-                            <a class="active" href="#">Home</a>
-                        </li>
-                    </ul>
+                    <text>
+                        <a href="#">Dashboard ></a> <a href="#">Home</a>
+                    </text>
                 </div>
 
             </div>
 
-            <ul class="box-info">
-                <li>
-                    <i class='bx bxs-calendar-check'></i>
-                    <span class="text">
-                        <h3>1020</h3>
-                        <p>New Reports</p>
-                    </span>
-                </li>
-                <li>
-                    <i class='bx bxs-group'></i>
-                    <span class="text">
-                        <h3>2834</h3>
-                        <p>Total Items</p>
-                    </span>
-                </li>
-                <li>
-                    <i class='bx bxs-check'></i>
-                    <span class="text">
-                        <h3>2543</h3>
-                        <p>Recovered Items</p>
-                    </span>
-                </li>
-            </ul>
-
-
-            <div class="table-data">
-                <div class="order">
-                    <div class="head">
-                        <h3>Recent Reports</h3>
-                        <i class='bx bx-search'></i>
-                        <i class='bx bx-filter'></i>
-                    </div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>User</th>
-                                <th>Date Reported</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <img src="img/people.png">
-                                    <p>John Doe</p>
-                                </td>
-                                <td>01-10-2021</td>
-                                <td><span class="status completed">Recovered</span></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <img src="img/people.png">
-                                    <p>John Doe</p>
-                                </td>
-                                <td>01-10-2021</td>
-                                <td><span class="status pending">Pending</span></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <img src="img/people.png">
-                                    <p>John Doe</p>
-                                </td>
-                                <td>01-10-2021</td>
-                                <td><span class="status process">Processing</span></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <img src="img/people.png">
-                                    <p>John Doe</p>
-                                </td>
-                                <td>01-10-2021</td>
-                                <td><span class="status pending">Pending</span></td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <img src="img/people.png">
-                                    <p>John Doe</p>
-                                </td>
-                                <td>01-10-2021</td>
-                                <td><span class="status completed">Recovered</span></td>
-                            </tr>
-                        </tbody>
-                    </table>
+            <section class = "stats">
+                <div class = "box">
+                    <h3> 1020</h3>
+                    <p>Found Items</p>
                 </div>
+                <div class = "box">
+                    <h3>1020</h3>
+                    <p>Lost Items</p>
+                </div>
+                <div class = "box">
+                    <h3>1020</h3>
+                    <p>Claimed Items</p>
+                </div>
+            </section>
 
-            </div>
+            <section class="recent-activities">
+                <h2>Recent Activities</h2>
+                <table>
+                    <thead>
+                         <tr>
+                            <th>User</th>
+                            <th>Date</th>
+                            <th>Activity</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>John Doe</td>
+                            <td>01-03-2024</td>
+                            <td>Reported a lost item</td>
+                        </tr>
+                    </tbody>
+            </section>
+            
         </main>
-        <!-- MAIN -->
-    </section>
-    <!-- CONTENT -->
 
-
-    <script src="../js/admin_dash_script.js"></script>
+       
+    
+    </div>
+    
 </body>
-
-</html>
