fe098ab4132eb514e495dedd372f06fab738f8cc
kiki-etc <kiki.etc@icloud.com>
2024-03-17 16:34:22 +0000

    user and admin, same styling
    User and Admin Dashboards now use the same css file "dash_style.css" for their style properties.

--- admin/admin_dash.php
+++ admin/admin_dash.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Dashboard</title>
-    <link rel="stylesheet" href="../css/admin_dash_style.css">
+    <link rel="stylesheet" href="../css/dash_style.css">
 
 
 </head>
