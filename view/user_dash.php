1eb5e04dd0e7deb73d271e830ce229ce8468114c
kiki-etc <kiki.etc@icloud.com>
2024-03-17 16:25:57 +0000

    Update user_dash.php

--- view/user_dash.php
+++ view/user_dash.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>User Dashboard</title>
-    <link rel="stylesheet" href="../css/user_dash_style_new.css">
+    <link rel="stylesheet" href="../css/user_dash_style.css">
 
 
 </head>
